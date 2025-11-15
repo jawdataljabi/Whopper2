042c7b5d74eb49813dd0920ec1373c3de9965edf
BryanTheBacon <bryanvu2004@gmail.com>
2025-11-15 16:29:56 -0500

    dependency hell

--- asl-text/main.py
+++ asl-text/main.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 # Load your trained LSTM model (from .keras file)
 model = tf.keras.models.load_model('my_model.keras')
